915dd081fe74cd3bd957a2b54dcb39d26f2d83ba
zhangxin158 <158484819@qq.com>
2022-06-22 15:30:09 +0800

    master

--- src/text_read.py
+++ src/text_read.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import cv2
 import shutil
 #文件test.txt
@@ -17,3 +18,37 @@ def read_num_txt():
 		num=num+1
 	files.close()
 	return num
+=======
+# coding: utf-8
+import os
+
+def createFilelist(images_path, text_save_path):
+    # 打开图片列表清单txt文件
+    file_name = open(text_save_path, "w")
+    # 查看文件夹下的图片
+    images_name = os.listdir(images_path)
+    # 遍历所有文件
+    for eachname in images_name:
+        # 按照需要的格式写入目标txt文件
+        file_name.write(images_path + '/' + eachname + '\n')
+        
+    print('生成txt成功！')
+
+    file_name.close()
+
+
+
+if __name__ == "__main__":
+    # txt文件存放目录
+    txt_path = r'C:\Users\15848\Desktop\dog_cat_classification\input' 
+    # 图片存放目录
+    images_path = r'C:\Users\15848\Desktop\AlexNet\data_name\Cat'
+    # 生成图片列表文件命名
+    txt_name = 'img_paths.txt'
+    if not os.path.exists(txt_name):
+        os.mkdir(txt_name)
+    # 生成图片列表文件的保存目录
+    text_save_path = txt_path + '/' + txt_name
+    #生成txt文件
+    createFilelist(images_path, text_save_path)
+>>>>>>> ff1a5f47fc24f8533962a06ba9065315c3522098
--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import torch
 from torch import nn
 from build_network import MyAlexNet
@@ -186,3 +187,185 @@ for t in range(epoch):
 
 
 print('Done!')
+=======
+import torch
+from torch import nn
+from build_network import MyAlexNet
+import numpy as np
+from torch.optim import lr_scheduler
+import os
+
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
+
+from shutil import copy
+import random
+
+
+
+ROOT_TRAIN = r'C:/Users/15848/Desktop/AlexNet/data/train'
+ROOT_TEST = r'C:/Users/15848/Desktop/AlexNet/data/val'
+#文件划分
+def mkfile(file):
+    if not os.path.exists(file):
+        os.makedirs(file)
+
+
+# 获取data文件夹下所有文件夹名（即需要分类的类名）
+file_path = 'C:/Users/15848/Desktop/AlexNet/data_name'
+flower_class = [cla for cla in os.listdir(file_path)]
+
+# 创建 训练集train 文件夹，并由类名在其目录下创建子目录
+mkfile('data/train')
+for cla in flower_class:
+    mkfile('data/train/' + cla)
+
+# 创建 验证集val 文件夹，并由类名在其目录下创建子目录
+mkfile('data/val')
+for cla in flower_class:
+    mkfile('data/val/' + cla)
+
+# 划分比例，训练集 : 验证集 = 8 : 2
+split_rate = 0.2
+
+# 遍历所有类别的全部图像并按比例分成训练集和验证集
+for cla in flower_class:
+    cla_path = file_path + '/' + cla + '/'  # 某一类别的子目录
+    images = os.listdir(cla_path)  # iamges 列表存储了该目录下所有图像的名称
+    num = len(images)
+    eval_index = random.sample(images, k=int(num * split_rate))  # 从images列表中随机抽取 k 个图像名称
+    for index, image in enumerate(images):
+        # eval_index 中保存验证集val的图像名称
+        if image in eval_index:
+            image_path = cla_path + image
+            new_path = 'data/val/' + cla
+            copy(image_path, new_path)  # 将选中的图像复制到新路径
+
+        # 其余的图像保存在训练集train中
+        else:
+            image_path = cla_path + image
+            new_path = 'data/train/' + cla
+            copy(image_path, new_path)
+        print("\r[{}] processing [{}/{}]".format(cla, index + 1, num), end="")  # processing bar
+    print()
+
+print("processing done!")
+
+#图像处理
+# 将图像的像素值归一化到【-1， 1】之间
+normalize = transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])
+
+train_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.RandomVerticalFlip(),
+    transforms.ToTensor(),
+    normalize])
+
+val_transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    normalize])
+
+train_dataset = ImageFolder(ROOT_TRAIN, transform=train_transform)
+val_dataset = ImageFolder(ROOT_TEST, transform=val_transform)
+
+train_dataloader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_dataloader = DataLoader(val_dataset, batch_size=32, shuffle=True)
+
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+model = MyAlexNet().to(device)
+
+# 定义一个损失函数
+loss_fn = nn.CrossEntropyLoss()
+
+# 定义一个优化器
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+# 学习率每隔10轮变为原来的0.5
+lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.5)
+
+# 定义训练函数
+def train(dataloader, model, loss_fn, optimizer):
+    loss, current, n = 0.0, 0.0, 0
+    for batch, (x, y) in enumerate(dataloader):
+        image, y = x.to(device), y.to(device)
+        output = model(image)
+        cur_loss = loss_fn(output, y)
+        _, pred = torch.max(output, axis=1)
+        cur_acc = torch.sum(y==pred) / output.shape[0]
+
+        # 反向传播
+        optimizer.zero_grad()
+        cur_loss.backward()
+        optimizer.step()
+        loss += cur_loss.item()
+        current += cur_acc.item()
+        n = n+1
+
+    train_loss = loss / n
+    train_acc = current / n
+    print('train_loss' + str(train_loss))
+    print('train_acc' + str(train_acc))
+    return train_loss, train_acc
+
+# 定义一个验证函数
+def val(dataloader, model, loss_fn):
+    # 将模型转化为验证模型
+    model.eval()
+    loss, current, n = 0.0, 0.0, 0
+    with torch.no_grad():
+        for batch, (x, y) in enumerate(dataloader):
+            image, y = x.to(device), y.to(device)
+            output = model(image)
+            cur_loss = loss_fn(output, y)
+            _, pred = torch.max(output, axis=1)   
+            cur_acc = torch.sum(y == pred) / output.shape[0]
+            loss += cur_loss.item()
+            current += cur_acc.item()
+            n = n + 1
+
+    val_loss = loss / n
+    val_acc = current / n
+    print('val_loss' + str(val_loss))
+    print('val_acc' + str(val_acc))
+    return val_loss, val_acc
+
+
+
+# 开始训练
+loss_train = []
+acc_train = []
+loss_val = []
+acc_val = []
+
+
+epoch = 10
+min_acc = 0
+for t in range(epoch):
+    lr_scheduler.step()
+    
+    print(f"epoch{t+1}\n-----------")
+    train_loss, train_acc = train(train_dataloader, model, loss_fn, optimizer)
+    val_loss, val_acc = val(val_dataloader, model, loss_fn)
+    
+    loss_train.append(train_loss)
+    acc_train.append(train_acc)
+    loss_val.append(val_loss)
+    acc_val.append(val_acc)
+
+    # 保存最好的模型权重
+    if val_acc >min_acc:
+        folder = 'weights'
+        if not os.path.exists(folder):
+            os.mkdir('weights')
+        min_acc = val_acc
+        print(f"save best model, 第{t+1}轮")
+        torch.save(model.state_dict(), 'weights/best_model.pth')
+
+
+print('Done!')
+>>>>>>> ff1a5f47fc24f8533962a06ba9065315c3522098
